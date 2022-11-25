e634f5246d194d82a395c2913dc6baf2f7aed0ea
siddhardha varma <sid@pop-os.localdomain>
2022-11-25 17:00:12 +0530

    faucet contract added

---
+++ scripts/deployFaucet.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Faucet = await hre.ethers.getContractFactory("Faucet");
+  const faucet = await Faucet.deploy("0xa97204FbF4C7F691FCD042b2b420FC1Db7DA0639");
+
+  await faucet.deployed();
+
+  console.log("Facuet contract deployed: ", faucet.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
